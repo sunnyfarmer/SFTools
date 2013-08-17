bf582d9516a44efcafc361774aca8b9ea3c6261c
Sunnyfarmer <84193866@qq.com>
2013-08-17 14:01:16 +0800

    添加商品清单页面

---
+++ SFPeddlers/src/sf/tools/peddlers/ActivityShopping.java
@@ -0,0 +1,30 @@
+package sf.tools.peddlers;
+
+import android.os.Bundle;
+
+public class ActivityShopping extends TopActivity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+	@Override
+	protected void initData() {
+		// TODO Auto-generated method stub
+		super.initData();
+	}
+	@Override
+	protected void initView() {
+		// TODO Auto-generated method stub
+		super.initView();
+	}
+	@Override
+	protected void setListener() {
+		// TODO Auto-generated method stub
+		super.setListener();
+	}
+}
